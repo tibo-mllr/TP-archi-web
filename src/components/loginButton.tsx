d2f19f8d7bacb7523d861df900c9a3bbbfab18dd
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2025-03-11 16:42:01 +0100

    refactor: Improve logout handling + document weird API behavior

--- src/components/loginButton.tsx
+++ src/components/loginButton.tsx
@@ -22,16 +22,25 @@ export function LoginButton(): ReactElement {
   }, []);
 
   async function logout(): Promise<void> {
-    await api.get("/logout").catch((response) => {
-      // 406 is expected - the /logout route actually returns a 302 towards gourmet.cours.quimerch.com/
-      // Axios is forced to honor this redirect, and so the logout request is forwarded to the URL
-      // Now of course this route is not able to return any JSON, as it is the front page for the prof's website ! It returns HTML.
-      // So the server responds with a 406: not able to return the requested content type.
-      if (response.status !== 406 && response.status !== 200) {
+    /* == Note on logout ==
+      1.Since the login cookie is httpOnly, we cannot delete it from JS - we *need* a response from the server with the appropriate Set-Cookie header to clear it.
+        The easiest way to do this is to make a request to the /logout route.
+      
+      2.The /logout API route actually returns a 302 towards gourmet.cours.quimerch.com/.
+        Axios is forced to honor this redirect (see https://github.com/axios/axios/issues/3924), and so the logout request is forwarded to the above URL.
+        Now of course this route is not able to return any JSON, as it is the front page for the prof's website ! It returns HTML.
+      
+      3.This would lead to a 406 Error we would not be able to silence, so we override the headers for this request specifically to accept any content type to avoid that
+        This is a bit of a hack, but it works.
+        Notably this means we pull the HTML for the prof's frontpage on each logout (couple kB of data).
+        But as it is not rendered this is not too big a deal. If we had control over the API and could prevent this 302 response we could avoid this.
+    */
+    await api
+      .get("/logout", { headers: { Accept: "*/*" } })
+      .catch(() => {
         console.error("Failed to logout");
-      }
-    });
-    setIsLoggedIn(false);
+      })
+      .finally(() => setIsLoggedIn(false));
   }
 
   if (isLoggedIn) {
