5071dfc8a0e23de1ffa859508b15d67015b98df6
Thibault Muller <thibault.muller19@gmail.com>
2025-03-11 11:40:29 +0100

    feat: improve header title and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,8 +9,16 @@ export function Header(): ReactElement {
   const pathname = usePathname();
 
   return (
-    <header className="dark:bg-gray-700 bg-gray-100 p-4">
-      <nav className="flex justify-between items-center">
+    <header
+      className="dark:bg-gray-700 bg-gray-100 p-4 items-center content-center"
+      style={{
+        maxHeight: "10vh",
+        paddingTop: "2vh",
+        paddingBottom: "2vh",
+        minHeight: "40px",
+      }}
+    >
+      <nav className="flex justify-between items-center relative">
         {/* Main nav */}
         <div>
           <Link
@@ -27,14 +35,20 @@ export function Header(): ReactElement {
           </Link>
         </div>
 
-        {/* Logo and site name */}
-        <div className="flex space-x-2 items-center">
+        {/* Logo and site name, centered inside the whole nav */}
+        <div className="flex space-x-2 items-center absolute left-1/2 -translate-x-1/2">
           <Image
             src="/logo.png"
             alt="Logo"
             width={212}
             height={212}
-            style={{ maxHeight: "8vh", maxWidth: "8vh", aspectRatio: 1 }}
+            style={{
+              maxHeight: "5vh",
+              maxWidth: "5vh",
+              aspectRatio: 1,
+              minHeight: "30px",
+              minWidth: "30px",
+            }}
           />
           <h1 className="text-2xl text-center inline-block">Sigma cooking</h1>
         </div>
