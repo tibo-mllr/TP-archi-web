7ce22e35f3dad3dd08472533b6ba5303e93fcd2e
Thibault Muller <thibault.muller19@gmail.com>
2025-03-13 14:41:13 +0100

    feat: responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,49 @@
 "use client";
 
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import type { ReactElement } from "react";
+import { useState, type ReactElement } from "react";
 
 import { LoginButton } from "./loginButton";
 
 export function Header(): ReactElement {
   const pathname = usePathname();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const [open, setOpen] = useState(false);
+
+  const navLinks: ReactElement = (
+    <>
+      {/* Main nav */}
+      <Link href="/">
+        <Button variant={pathname == "/" ? "contained" : "outlined"}>
+          Home
+        </Button>
+      </Link>
+      <Link href="/favorites">
+        <Button variant={pathname == "/favorites" ? "contained" : "outlined"}>
+          Favorites
+        </Button>
+      </Link>
+
+      {/* Auth nav */}
+      <LoginButton />
+    </>
+  );
 
   return (
     <AppBar position="static" color="inherit" enableColorOnDark>
@@ -23,20 +57,35 @@ export function Header(): ReactElement {
         <Image src="/logo.png" alt="Logo" width={60} height={60} />
         <Typography variant="h5">Sigma cooking</Typography>
 
-        {/* Main nav */}
-        <Link href="/">
-          <Button variant={pathname == "/" ? "contained" : "outlined"}>
-            Home
-          </Button>
-        </Link>
-        <Link href="/favorites">
-          <Button variant={pathname == "/favorites" ? "contained" : "outlined"}>
-            Favorites
-          </Button>
-        </Link>
-
-        {/* Auth nav */}
-        <LoginButton />
+        {/* Navigation */}
+        {isMobile ? (
+          <>
+            <IconButton
+              color="inherit"
+              onClick={() => setOpen(true)}
+              sx={{ marginLeft: "auto" }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
+              <Box
+                sx={{
+                  width: "35vw",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: 2,
+                  padding: 3,
+                }}
+                role="presentation"
+                onClick={() => setOpen(false)}
+              >
+                {navLinks}
+              </Box>
+            </Drawer>
+          </>
+        ) : (
+          navLinks
+        )}
       </Toolbar>
     </AppBar>
   );
