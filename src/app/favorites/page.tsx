767b3a73395c4281a5f751356164d6a778362080
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2025-03-25 15:24:44 +0100

    feat: Basic favorites page

---
+++ src/app/favorites/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { List, ListItem } from "@mui/material";
+import { redirect } from "next/navigation";
+import { ReactElement, useState } from "react";
+
+import { RecipeCard } from "@/components";
+import { api } from "@/lib";
+import { Recipe } from "@/lib/types";
+
+function getFavorites(): Promise<Recipe[]> {
+  return (
+    api("/favorites")
+      .then((response) => response.data)
+      // Redirect to /login in case of 401
+      .catch((error) => {
+        if (error.response.status == 401) {
+          redirect("/login");
+        }
+      })
+  );
+}
+
+export default function FavoritesPage(): ReactElement {
+  // Now we know we are authenticated
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  getFavorites().then((recipes) => setRecipes(recipes));
+
+  return (
+    <List>
+      {recipes == null ? (
+        <p>Tu n&apos;as pas de recettes favories !</p>
+      ) : (
+        recipes.map((recipe) => (
+          <ListItem key={recipe.id}>
+            <RecipeCard recipe={recipe} />
+          </ListItem>
+        ))
+      )}
+    </List>
+  );
+}
