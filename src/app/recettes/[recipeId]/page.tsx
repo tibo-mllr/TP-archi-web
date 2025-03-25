c612c32525132d79b4d40d38c4c20fed7783861e
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2025-03-25 15:36:19 +0100

    feat: Change dish type display to fit in better

--- src/app/recettes/[recipeId]/page.tsx
+++ src/app/recettes/[recipeId]/page.tsx
@@ -3,6 +3,7 @@ import AvTimerIcon from "@mui/icons-material/AvTimer";
 import ClassIcon from "@mui/icons-material/Class";
 import EuroIcon from "@mui/icons-material/Euro";
 import PeopleIcon from "@mui/icons-material/People";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
 import {
   Card,
@@ -26,6 +27,7 @@ type RecipePageProps = {
   params: Promise<{ recipeId: string }>;
 };
 
+// TODO: Move to different file probably
 export async function generateMetadata(
   { params }: RecipePageProps,
   parent: ResolvingMetadata,
@@ -43,6 +45,7 @@ export async function generateMetadata(
   };
 }
 
+// TODO: Ditto
 function parseInstructions(instructions?: string): ReactElement {
   const lines = instructions?.split("\n");
   let isList = false;
@@ -138,11 +141,12 @@ export default async function RecipePage({
           <Grid2 size={{ xs: 12, sm: 4, md: 4 }}>
             <List>
               <ListItem>
+                <ListItemIcon>
+                  <RestaurantIcon />
+                </ListItemIcon>
                 <ListItemText>
-                  <Typography variant="h6">
-                    {capitalizeFirstLetter(when_to_eat ?? "") ||
-                      "Eat whenever you want"}
-                  </Typography>
+                  <b>Type: </b>
+                  {capitalizeFirstLetter(when_to_eat || "Misc.")}
                 </ListItemText>
               </ListItem>
               {!!category && (
