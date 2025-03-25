443a41e0e165250211b482a06363922de178c36c
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2025-03-25 16:40:42 +0100

    feat: Style add to favorites button

--- src/app/recettes/[recipeId]/ui/AddToFavoritesButton.tsx
+++ src/app/recettes/[recipeId]/ui/AddToFavoritesButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 // Use client necessary because we want to make authenticated calls from client
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
 import Button from "@mui/material/Button";
 import { ReactElement } from "react";
 
@@ -16,14 +16,20 @@ export default function AddToFavoritesButton({
   recipe,
 }: AddToFavoritesButtonProps): ReactElement {
   function addRecipeToFavorites(): void {
+    // TODO: Do this properly, by getting the username of the currently loggedin user
     api.post("/users/sigma/favorites", {}, { params: { recipeID: recipe.id } });
   }
 
   return (
     <Button
       variant="contained"
-      color="primary"
-      startIcon={<FavoriteIcon />}
+      sx={{
+        backgroundColor: "#F75D8F",
+        color: "white",
+        "&:hover": { backgroundColor: "#FF407F" },
+        textAlign: "center",
+      }}
+      startIcon={<FavoriteBorderRoundedIcon />}
       onClick={addRecipeToFavorites}
     >
       Add to Favorites
