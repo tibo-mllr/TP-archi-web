3dfd82315817b3f96a622294c6c0835aa32acad8
Thibault Muller <thibault.muller19@gmail.com>
2025-04-06 19:57:43 +0200

    feat: only transparent in "/" page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,11 +48,15 @@ export function Header(): ReactElement {
   return (
     <AppBar
       position="sticky"
-      sx={{
-        backgroundColor: "rgba(55, 55, 55, 0.6)",
-        color: "white",
-        backgroundImage: "none",
-      }}
+      {...(pathname === "/"
+        ? {
+            sx: {
+              backgroundColor: "rgba(55, 55, 55, 0.6)",
+              color: "white",
+              backgroundImage: "none",
+            },
+          }
+        : { color: "inherit" })}
       enableColorOnDark
     >
       <Toolbar
