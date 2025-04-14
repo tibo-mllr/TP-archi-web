8daf1b23ef190666cab4e1f1704d6ca6fe49e1f6
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2025-04-14 15:16:32 +0200

    feat: Create cookie holding loggedIn state to avoid having to request the API every time

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,3 +1,19 @@
 export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
+
+type JWTContent = {
+  iss: string;
+  sub: string;
+  exp: number;
+  iat: number;
+  jti: string;
+  roles: string[];
+};
+
+export function parseJwt(token: string): JWTContent {
+  // eslint-disable-next-line prefer-destructuring
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace("-", "+").replace("_", "/");
+  return JSON.parse(window.atob(base64));
+}
